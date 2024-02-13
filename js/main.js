5a2e84187b35e4242eaf476e7a1ff2c9b85e1ea4
BillOfBalrog <boaz.teller@gmail.com>
2024-02-13 23:43:33 +0200

    Added 2nd ball. Added maxDiameter parameter to limit max ball size in function.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
 'use strict'
 
-function onBallClick(elBall) {
+function onBallClick(elBall, maxDiameter) {
     const currSize = elBall.offsetWidth
-    const newSize = currSize + getRandomIntInclusive(20, 60)
-    if (currSize === 400) newSize = 100              
-
+    const newSize = currSize > maxDiameter ? 100 : currSize + getRandomIntInclusive(20, 60)
+    
     elBall.style.backgroundColor = getRandomColor()
-
+    elBall.innerText = newSize
     elBall.style.height = newSize + 'px'
     elBall.style.width = newSize + 'px'
 }      
