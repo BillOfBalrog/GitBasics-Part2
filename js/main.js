e60dae6f9147c15b34a78e1619495dc37f1e0976
BillOfBalrog <boaz.teller@gmail.com>
2024-02-14 00:31:09 +0200

    Added Section1

--- js/main.js
+++ js/main.js
@@ -10,3 +10,20 @@ function onBallClick(elBall, maxDiameter) {
     elBall.style.width = newSize + 'px'
 }      
 
+function onBallClick3(elBall) {
+    const ball1Element = document.querySelector('.ball1');
+    const ball2Element = document.querySelector('.ball2');
+
+    const ball1Color = window.getComputedStyle(ball1Element).backgroundColor;
+    const ball2Color = window.getComputedStyle(ball2Element).backgroundColor;
+    const ball1Size = ball1Element.offsetWidth
+    const ball2Size = ball2Element.offsetWidth
+
+    ball1Element.style.backgroundColor = ball2Color
+    ball2Element.style.backgroundColor = ball1Color
+
+    ball1Element.style.width = ball1Element.style.height = ball2Size + 'px'
+    ball2Element.style.width = ball2Element.style.height = ball1Size + 'px'
+
+}                                          
+
