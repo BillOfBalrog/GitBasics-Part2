5d14f3350e0a8d7486e9ed479dbeb9e2c151e944
BillOfBalrog <boaz.teller@gmail.com>
2024-02-13 22:23:03 +0200

    Added getRandomIntInclusive(20, 60) to ball size on click function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 
 function onBallClick(elBall) {
     const currSize = elBall.offsetWidth
-    const newSize = currSize === 400 ? 100 : currSize + 50 
+    const newSize = currSize + getRandomIntInclusive(20, 60)
+    if (currSize === 400) newSize = 100              
 
     elBall.style.height = newSize + 'px'
     elBall.style.width = newSize + 'px'
