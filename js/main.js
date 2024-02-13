e8e9bb7e67db438bf0860a670fea6378ea994039
BillOfBalrog <boaz.teller@gmail.com>
2024-02-13 22:33:30 +0200

    Added random color onBallClick function and in utils.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ function onBallClick(elBall) {
     const newSize = currSize + getRandomIntInclusive(20, 60)
     if (currSize === 400) newSize = 100              
 
+    elBall.style.backgroundColor = getRandomColor()
+
     elBall.style.height = newSize + 'px'
     elBall.style.width = newSize + 'px'
 }      
