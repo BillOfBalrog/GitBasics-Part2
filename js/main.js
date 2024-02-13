a7a832dd5b340f320b614f45627ffc902db77a17
BillOfBalrog <boaz.teller@gmail.com>
2024-02-13 21:57:55 +0200

    Limit ball size to 400px in onBallClick function.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 function onBallClick(elBall) {
     const currSize = elBall.offsetWidth
-    const newSize = currSize + 50       
+    const newSize = currSize === 400 ? 100 : currSize + 50 
 
     elBall.style.height = newSize + 'px'
     elBall.style.width = newSize + 'px'
